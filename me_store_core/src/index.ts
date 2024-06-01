d9f2028be95f92518019cdd25e7d22ff3b683406
Lo King Wei <kingwei.lo@live.com.my>
2024-06-01 14:12:35 +0800

    wip: test build apk

--- me_store_core/src/index.ts
+++ me_store_core/src/index.ts
@@ -11,3 +11,21 @@ export interface MeStoreReleaseInfo {
 
   installerUrl: string;
 }
+
+import { Hono } from 'hono'
+
+type Bindings = {
+  OPENAI_API_KEY: string
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/', (c) => {
+  return c.text(c.env.OPENAI_API_KEY);
+})
+
+app.get('/version', (c) => {
+  return c.text(c.env.OPENAI_API_KEY);
+})
+
+export default app
